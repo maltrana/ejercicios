57c493e356e8ad11a7d24796b735cbaa327d9d14
fer <fer@ticmedia.com>
2017-11-12 21:23:14 +0100

    inicio

---
+++ multiplica.php
@@ -0,0 +1,15 @@
+<?php
+
+class multiplica {
+
+
+    function __constructor( ) {
+
+    }
+
+
+    function multiplica($a= 0, $b= 0) {
+        return (a * $b);
+    }
+
+}
---
+++ narcisista.php
@@ -0,0 +1,25 @@
+<?php
+
+/* 
+ Un número narcisista es un número de n dígitos, que coincide con la suma de las potencias n-ésimas de sus dígitos. ... 
+  
+ Los primeros números narcisistas son 1, 2, ....  8208 = 8^4+2^4+0^4+8^4
+ 
+
+ */
+
+class narcisista {
+
+
+    function __constructor( ) {
+
+    }
+
+
+    function es_narcisista($numero) {
+        return (true);
+    }
+
+}
+
+
